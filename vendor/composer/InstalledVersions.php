45fcec993684296ad27a5cfa5479b804aba537d3
Jaspreet Singh <jaspreetssekhon@gmail.com>
2021-02-28 06:13:26 +0530

    Bootstrap
    Installed Boostrap Lib

--- vendor/composer/InstalledVersions.php
+++ vendor/composer/InstalledVersions.php
@@ -30,7 +30,7 @@ private static $installed = array (
     'aliases' => 
     array (
     ),
-    'reference' => 'ced1926f3992bd6a9e7cf0da8b4df3efa16beb82',
+    'reference' => 'dab590470a1660ec58222b2d2730342a5675537a',
     'name' => '__root__',
   ),
   'versions' => 
@@ -42,7 +42,7 @@ private static $installed = array (
       'aliases' => 
       array (
       ),
-      'reference' => 'ced1926f3992bd6a9e7cf0da8b4df3efa16beb82',
+      'reference' => 'dab590470a1660ec58222b2d2730342a5675537a',
     ),
     'phpmailer/phpmailer' => 
     array (
@@ -52,6 +52,22 @@ private static $installed = array (
       array (
       ),
       'reference' => '4a08cf4cdd2c38d12ee2b9fa69e5d235f37a6dcb',
+    ),
+    'twbs/bootstrap' => 
+    array (
+      'pretty_version' => 'v4.6.0',
+      'version' => '4.6.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6ffb0b48e455430f8a5359ed689ad64c1143fac2',
+    ),
+    'twitter/bootstrap' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v4.6.0',
+      ),
     ),
   ),
 );
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -6,7 +6,7 @@
     'aliases' => 
     array (
     ),
-    'reference' => 'ced1926f3992bd6a9e7cf0da8b4df3efa16beb82',
+    'reference' => 'dab590470a1660ec58222b2d2730342a5675537a',
     'name' => '__root__',
   ),
   'versions' => 
@@ -18,7 +18,7 @@
       'aliases' => 
       array (
       ),
-      'reference' => 'ced1926f3992bd6a9e7cf0da8b4df3efa16beb82',
+      'reference' => 'dab590470a1660ec58222b2d2730342a5675537a',
     ),
     'phpmailer/phpmailer' => 
     array (
@@ -28,6 +28,22 @@
       array (
       ),
       'reference' => '4a08cf4cdd2c38d12ee2b9fa69e5d235f37a6dcb',
+    ),
+    'twbs/bootstrap' => 
+    array (
+      'pretty_version' => 'v4.6.0',
+      'version' => '4.6.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6ffb0b48e455430f8a5359ed689ad64c1143fac2',
+    ),
+    'twitter/bootstrap' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v4.6.0',
+      ),
     ),
   ),
 );
